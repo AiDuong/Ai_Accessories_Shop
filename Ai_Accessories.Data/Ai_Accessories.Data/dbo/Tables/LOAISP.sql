823d2d9a7f051304fde1c391b0c8a3eac365bfd4
Duong Stephen <stephenduong@outlook.com.vn>
2019-03-23 13:48:50 +0700

    Updated Data

--- Ai_Accessories.Data/Ai_Accessories.Data/dbo/Tables/LOAISP.sql
+++ Ai_Accessories.Data/Ai_Accessories.Data/dbo/Tables/LOAISP.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[LOAISP] (
     [Id]        INT           IDENTITY (1, 1) NOT NULL,
     [TenloaiSP] NVARCHAR (50) NULL,
+    [Flag]      VARCHAR (100) NULL,
     CONSTRAINT [PK_LOAISP] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
