34d3bb8353d885bf42d8292519ba5754cc3a43d8
Duong Stephen <stephenduong@outlook.com.vn>
2019-05-14 02:53:24 +0700

    compare database nha

--- Ai_Accessories.Data/Ai_Accessories.Data/dbo/Tables/CLIENTACCOUNT.sql
+++ Ai_Accessories.Data/Ai_Accessories.Data/dbo/Tables/CLIENTACCOUNT.sql
@@ -1,13 +1,14 @@
 ﻿CREATE TABLE [dbo].[CLIENTACCOUNT] (
-    [Id]          INT            NOT NULL,
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
     [AccountName] NVARCHAR (50)  NOT NULL,
     [PassWord]    NVARCHAR (50)  NOT NULL,
     [ClientName]  NVARCHAR (255) NOT NULL,
-    [Gender]      BIT            NULL,
     [PhoneNumber] NCHAR (11)     NULL,
     [Email]       NVARCHAR (50)  NULL,
     [Address]     NVARCHAR (255) NULL,
     [Note]        NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_ClientAccount] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
